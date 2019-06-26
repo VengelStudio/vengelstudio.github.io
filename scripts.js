7f1d89bfb8a2c326c3218080458e1a77190a3ae9
Kartm <lblachnicki@gmail.com>
2019-06-26 14:51:49 +0200

    Add icon authors in footer, fix fog being in front of the logo, add basic project info

---
+++ scripts.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.section.project').forEach((project, i) => {
+        project
+            .querySelector('.project-title')
+            .addEventListener('click', () => {
+                project.querySelector('.project-logo').style.display = 'none'
+                project.querySelector('.project-title').style.display = 'none'
+                project.querySelector('.project-description').style.display =
+                    'flex'
+            })
+        project
+            .querySelector('.project-description button:last-child')
+            .addEventListener('click', () => {
+                project.querySelector('.project-logo').style.display = 'flex'
+                project.querySelector('.project-title').style.display = 'flex'
+                project.querySelector('.project-description').style.display =
+                    'none'
+            })
+    })
+})
