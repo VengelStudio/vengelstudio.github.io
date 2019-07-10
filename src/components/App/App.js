e7d0be44b89e4f7d801120bad4d7892f843e8ef8
Bartosz Kępka <bartoszkepka1999@gmail.com>
2019-07-10 15:04:36 +0200

    make two bars in us section with images and icons

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import Page from '../Page/Page'
 import './App.scss'
-
+import bartek from "../../images/bartek.svg"
+import lukasz from "../../images/lukasz.svg"
+import github from "../../images/icons/github.svg"
+import linkedin from "../../images/icons/linkedin.svg"
+import email from "../../images/icons/email.svg"
 
 const App = () => {
     return (
@@ -18,8 +22,26 @@ const App = () => {
             <Page>
                 <div className="us-content">
                     <div className="two-bars">
-                        <div className="left-bar"></div>
-                        <div className="right-bar"></div>
+                        <div className="left-bar">
+                            <img className='logo' src={bartek}></img>
+                            <p className='name'>Bartosz Kępka</p>
+                            <p>Automation and robotics student in Warsaw.</p>
+                            <div className="icons">
+                                <a href="mailto:bartoszkepka1999@gmail.com"><img src={email}></img></a>
+                                <a href="https://www.linkedin.com/in/bartosz-k%C4%99pka-118325181/"><img src={linkedin}></img></a>
+                                <a href="https://github.com/Dezann"><img src={github}></img></a>
+                            </div>
+                        </div>
+                        <div className="right-bar">
+                            <img className='logo' src={lukasz}></img>
+                            <p className='name'>Łukasz Blachnicki</p>
+                            <p>Applied computer science student in Wroclaw.</p>
+                            <div className="icons">
+                                <a href="mailto:lblachnicki@gmail.com"><img src={email}></img></a>
+                                <a href="https://www.linkedin.com/in/%C5%82ukasz-blachnicki/"><img src={linkedin}></img></a>
+                                <a href="https://github.com/Kartm"><img src={github}></img></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <p className="footer">© 2019 Vengel Studio. All rights reserved. Icons made by Freepik.</p>
