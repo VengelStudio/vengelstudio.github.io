d96f3a60cb9ce09bc78e00fdb0ab2831d8062a6b
Bartosz Kępka <bartoszkepka1999@gmail.com>
2019-07-10 14:26:52 +0200

    make two bars in us section

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Page from '../Page/Page'
 import './App.scss'
 
+
 const App = () => {
     return (
         <div className='main'>
@@ -14,7 +15,15 @@ const App = () => {
                 </div>
             </Page>
             <Page>projects</Page>
-            <Page>about us</Page>
+            <Page>
+                <div className="us-content">
+                    <div className="two-bars">
+                        <div className="left-bar"></div>
+                        <div className="right-bar"></div>
+                    </div>
+                </div>
+                <p className="footer">© 2019 Vengel Studio. All rights reserved. Icons made by Freepik.</p>
+            </Page>
         </div>
     )
 }
