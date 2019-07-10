2a8fb10a3ccf547b044154fc51186cde1a51e4ed
Kartm <lblachnicki@gmail.com>
2019-07-10 10:16:21 +0200

    Add SASS support, add basic layout elements

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Page from '../Page/Page'
+import './App.scss'
+
+function App() {
+    return (
+        <div className='main'>
+            <Page />
+            <Page />
+            <Page />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Page/Page.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Page.scss'
+
+function Page() {
+    return <div className='page'>test content</div>
+}
+
+export default Page
