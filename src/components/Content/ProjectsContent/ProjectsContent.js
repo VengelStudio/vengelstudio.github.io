3d81c8665d61273001b06754c9f6f2c5116baf34
Kartm <lblachnicki@gmail.com>
2019-07-12 00:41:19 +0200

    Add horizontal sashes and basic project layout

--- src/components/Content/ProjectsContent/ProjectsContent.js
+++ src/components/Content/ProjectsContent/ProjectsContent.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import './ProjectsContent.scss'
+import HorizontalSash from '../../Sashes/HorizontalSash/HorizontalSash'
+import BookmarkPurifierLogo from '../../../images/logos/bookmark-purifier.svg'
+import CardmanLogo from '../../../images/logos/cardman-multiplayer.svg'
+import PoznajmySieLogo from '../../../images/logos/poznajmy-sie.svg'
 
 const ProjectsContent = () => {
-    return <div className='projects-content'>empty</div>
+    return (
+        <div className='projects-content'>
+            <HorizontalSash bgColor={'#3F3F3F'} backColor={'#0F0F0F'}>
+                <img src={CardmanLogo} />
+                <span>Cardman Multiplayer</span>
+            </HorizontalSash>
+            <HorizontalSash bgColor={'#457D52'} backColor={'#174D24'}>
+                <span>Bookmark Checker</span>
+                <img src={BookmarkPurifierLogo} />
+            </HorizontalSash>
+            <HorizontalSash bgColor={'#8D3A63'} backColor={'#551E39'}>
+                <img src={PoznajmySieLogo} />
+                <span>Poznajmy się</span>
+            </HorizontalSash>
+        </div>
+    )
 }
 
 export default ProjectsContent
