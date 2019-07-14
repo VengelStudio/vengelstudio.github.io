2860cb2e09834f10f204571be93bf8f252114259
Kartm <lblachnicki@gmail.com>
2019-07-14 22:29:31 +0200

    Add basic us mobile layout

--- src/components/Content/UsContent/UsContent.js
+++ src/components/Content/UsContent/UsContent.js
@@ -11,40 +11,38 @@ const UsContent = () => {
     return (
         <React.Fragment>
             <div className='us-content'>
-                <div className='two-bars'>
-                    <VerticalSash>
-                        <img className='logo' src={bartek} alt='' />
-                        <p className='name'>Bartosz Kępka</p>
-                        <p>Automation and robotics student in Warsaw.</p>
-                        <div className='icons'>
-                            <a href='mailto:bartoszkepka1999@gmail.com'>
-                                <img src={email} alt='' />
-                            </a>
-                            <a href='https://www.linkedin.com/in/bartosz-k%C4%99pka-118325181/'>
-                                <img src={linkedin} alt='' />
-                            </a>
-                            <a href='https://github.com/Dezann'>
-                                <img src={github} alt='' />
-                            </a>
-                        </div>
-                    </VerticalSash>
-                    <VerticalSash>
-                        <img className='logo' src={lukasz} alt='' />
-                        <p className='name'>Łukasz Blachnicki</p>
-                        <p>Applied computer science student in Wroclaw.</p>
-                        <div className='icons'>
-                            <a href='mailto:lblachnicki@gmail.com'>
-                                <img src={email} alt='' />
-                            </a>
-                            <a href='https://www.linkedin.com/in/%C5%82ukasz-blachnicki/'>
-                                <img src={linkedin} alt='' />
-                            </a>
-                            <a href='https://github.com/Kartm'>
-                                <img src={github} alt='' />
-                            </a>
-                        </div>
-                    </VerticalSash>
-                </div>
+                <VerticalSash>
+                    <img className='logo' src={bartek} alt='' />
+                    <p className='name'>Bartosz Kępka</p>
+                    <p>Automation and robotics student in Warsaw.</p>
+                    <div className='icons'>
+                        <a href='mailto:bartoszkepka1999@gmail.com'>
+                            <img src={email} alt='' />
+                        </a>
+                        <a href='https://www.linkedin.com/in/bartosz-k%C4%99pka-118325181/'>
+                            <img src={linkedin} alt='' />
+                        </a>
+                        <a href='https://github.com/Dezann'>
+                            <img src={github} alt='' />
+                        </a>
+                    </div>
+                </VerticalSash>
+                <VerticalSash>
+                    <img className='logo' src={lukasz} alt='' />
+                    <p className='name'>Łukasz Blachnicki</p>
+                    <p>Applied computer science student in Wroclaw.</p>
+                    <div className='icons'>
+                        <a href='mailto:lblachnicki@gmail.com'>
+                            <img src={email} alt='' />
+                        </a>
+                        <a href='https://www.linkedin.com/in/%C5%82ukasz-blachnicki/'>
+                            <img src={linkedin} alt='' />
+                        </a>
+                        <a href='https://github.com/Kartm'>
+                            <img src={github} alt='' />
+                        </a>
+                    </div>
+                </VerticalSash>
             </div>
             <div className='footer'>
                 © 2019 Vengel Studio. All rights reserved. Icons made by
