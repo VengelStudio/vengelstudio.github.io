9d2b3fc155db8653e4cc4aa60d73fda14c6c63f9
Łukasz Blachnicki <lblachnicki@gmail.com>
2019-11-09 05:07:18 +0100

    Add on scroll handlers

--- src/components/ParallaxBg/ParallaxBg.js
+++ src/components/ParallaxBg/ParallaxBg.js
@@ -1,12 +1,37 @@
 import React from "react";
 import "./ParallaxBg.scss";
 
-const ParallaxBg = () => {
-  return (
-    <div className="background">
-      <img src="/static/bg.png" />
-    </div>
-  );
-};
+// config
+const scrollZoomTrigger = 50; // in px
+
+class ParallaxBg extends React.Component {
+  state = {
+    isZoomed: false
+  };
+
+  handleScroll = e => {
+    const { scrollTop } = e.target;
+    this.setState({ isZoomed: scrollTop > scrollZoomTrigger });
+  };
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll, true);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll, true);
+  }
+
+  render() {
+    const getClasses = () => {
+      return `background ${this.state.isZoomed ? "zoomed" : ""}`;
+    };
+    return (
+      <div className={getClasses()}>
+        <img src="/static/bg.png" />
+      </div>
+    );
+  }
+}
 
 export default ParallaxBg;
