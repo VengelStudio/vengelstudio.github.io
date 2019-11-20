0bb963743f7a7f35515aab50f58c8bc2353a424c
Łukasz Blachnicki <lblachnicki@gmail.com>
2019-11-20 08:26:02 +0100

    Add PropTypes

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Text.scss";
+import PropTypes from "prop-types";
 
 export const Text = props => {
   let text = React.createElement(
@@ -8,6 +9,14 @@ export const Text = props => {
     props.children
   );
   return <React.Fragment>{text}</React.Fragment>;
+};
+Text.propTypes = {
+  size: PropTypes.number,
+  weight: PropTypes.number
+};
+Text.defaultProps = {
+  size: 4,
+  weight: 300
 };
 
 export default Text;
