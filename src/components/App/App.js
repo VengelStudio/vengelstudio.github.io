d887ec484e5ce346fc6797f602fb91e879a64a55
Łukasz Blachnicki <lblachnicki@gmail.com>
2019-11-20 10:23:25 +0100

    Move Projects to a seperate component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,9 +6,8 @@ import VSheader from "../VSheader/VSheader";
 import Navbar from "../Navbar/Navbar";
 import Hero from "../Hero/Hero";
 import Text from "../Text/Text";
-import ProjectCard from "../ProjectCard/ProjectCard";
-import Cardman from "../../assets/images/projects/cardman.png";
 import ContactSection from "../Sections/ContactSection/ContactSection";
+import ProjectsSection from "../Sections/ProjectsSection/ProjectsSection";
 
 const App = () => {
   return (
@@ -18,42 +17,7 @@ const App = () => {
         <Hero>We are Vengel Studio</Hero>
       </FullHeightWrapper>
       <FullHeightWrapper>
-        <VSheader size={2} shadow={true}>
-          Test header
-        </VSheader>
-        <br></br>
-        <Text size={1} weight={200}>
-          Test text
-        </Text>
-        <Text size={2} weight={700}>
-          Test text
-        </Text>
-        <Text size={3} weight={400}>
-          Test text
-        </Text>
-      </FullHeightWrapper>
-      <FullHeightWrapper>
-        <ProjectCard
-          title={"Cardman Multiplayer"}
-          description={
-            "A combination of a traditional hangman game and a card game"
-          }
-          screenshot={Cardman}
-        />
-        <ProjectCard
-          title={"Cardman Multiplayer"}
-          description={
-            "A combination of a traditional hangman game and a card game"
-          }
-          screenshot={Cardman}
-        />
-        <ProjectCard
-          title={"Cardman Multiplayer"}
-          description={
-            "A combination of a traditional hangman game and a card game"
-          }
-          screenshot={Cardman}
-        />
+        <ProjectsSection />
       </FullHeightWrapper>
       <FullHeightWrapper>
         <ContactSection />
