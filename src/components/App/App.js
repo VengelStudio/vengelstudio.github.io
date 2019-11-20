9b546aee2578a1145a5f6d60ee0e5865b332c8f3
Łukasz Blachnicki <lblachnicki@gmail.com>
2019-11-20 10:29:34 +0100

    Remove unnecessary imports

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,8 @@ import React from "react";
 import "./App.scss";
 import ParallaxContainer from "../ParallaxContainer/ParallaxContainer";
 import FullHeightWrapper from "../FullHeightWrapper/FullHeightWrapper";
-import VSheader from "../VSheader/VSheader";
 import Navbar from "../Navbar/Navbar";
 import Hero from "../Hero/Hero";
-import Text from "../Text/Text";
 import ContactSection from "../Sections/ContactSection/ContactSection";
 import ProjectsSection from "../Sections/ProjectsSection/ProjectsSection";
 
@@ -16,9 +14,11 @@ const App = () => {
       <FullHeightWrapper>
         <Hero>We are Vengel Studio</Hero>
       </FullHeightWrapper>
+
       <FullHeightWrapper>
         <ProjectsSection />
       </FullHeightWrapper>
+
       <FullHeightWrapper>
         <ContactSection />
       </FullHeightWrapper>
