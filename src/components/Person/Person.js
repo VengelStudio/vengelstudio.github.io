1ca6525c64aa1fdd71e252137a30ba782930de06
Łukasz Blachnicki <lblachnicki@gmail.com>
2019-11-20 10:56:42 +0100

    Add icons for Contact section

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -2,27 +2,42 @@ import React from "react";
 import "./Person.scss";
 import Text from "../Shared/Text/Text";
 import VSheader from "../Shared/Header/Header";
+import Icon from "../Shared/Icon/Icon";
 
-class Person extends React.Component {
-  render() {
-    return (
-      <div className="person">
-        <div className="person-info">
-          <VSheader size={3}>
-            {this.props.name}
-            <br />
-            {this.props.surname}
-          </VSheader>
-          <br></br>
-          <Text size={3}>{this.props.description}</Text>
-          <div className="icons">icons go here</div>
-        </div>
-        <div className="person-img">
-          <img src={this.props.imgUrl}></img>
+const Person = props => {
+  const getClickableIcons = () => {
+    let clickableIcons = [];
+    props.iconsWithLinks.forEach(element => {
+      clickableIcons.push(
+        <a href={element.goToUrl}>
+          <Icon url={element.iconUrl}></Icon>
+        </a>
+      );
+    });
+    return clickableIcons;
+  };
+
+  return (
+    <div className="person">
+      <div className="person-info">
+        <VSheader size={3}>
+          {props.name}
+          <br />
+          {props.surname}
+        </VSheader>
+        <br></br>
+        <Text size={3}>{props.description}</Text>
+        <div className="person-icons">
+          {getClickableIcons().map(el => (
+            <div>{el}</div>
+          ))}
         </div>
       </div>
-    );
-  }
-}
+      <div className="person-img">
+        <img src={props.imgUrl}></img>
+      </div>
+    </div>
+  );
+};
 
 export default Person;
