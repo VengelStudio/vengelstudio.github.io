494fae72f85c92a40154a08a3ce6eedfb33346b4
Łukasz Blachnicki <lblachnicki@gmail.com>
2019-11-25 08:17:16 +0100

    Add basic links section

---
+++ src/components/Sections/LinksSection/LinksSection.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./LinksSection.scss";
+import VSheader from "../../Shared/Header/Header";
+
+const LinksSection = () => {
+  return (
+    <section className="contact">
+      <VSheader size={4} shadow={true}>
+        Useful links
+      </VSheader>
+      linx
+    </section>
+  );
+};
+
+export default LinksSection;
