d4d92999d79a0ec7160f240fd1cdda640be1cd44
Bartosz Kępka <bartoszkepka1999@gmail.com>
2019-12-09 08:26:11 +0100

    Add animations in contact section

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Person.scss";
+import { TweenMax, Linear, TimelineMax } from "gsap";
 import Text from "../Shared/Text/Text";
 import Header from "../Shared/Header/Header";
 import Icon from "../Shared/Icon/Icon";
 
 const Person = props => {
+  const tl = new TimelineMax({ delay: 0 });
+  let personImg = useRef(null);
+  let contactBar = useRef(null);
+
+  useEffect(() => {
+    tl.set(personImg, { opacity: 0 });
+    tl.fromTo(contactBar, 1, { x: -12000, ease: Linear.ease }, { x: 0 }).fromTo(
+      personImg,
+      1,
+      { x: 900, opacity: 0, ease: Linear.ease },
+      { x: 0, opacity: 1, ease: Linear.ease }
+    );
+  });
+
   const getClickableIcons = () => {
     let clickableIcons = [];
     props.iconsWithLinks.forEach(element => {
@@ -18,7 +33,12 @@ const Person = props => {
   };
 
   return (
-    <div className="person">
+    <div
+      className="person"
+      ref={element => {
+        contactBar = element;
+      }}
+    >
       <div className="person-info">
         <Header size={3}>
           {props.name}
@@ -33,7 +53,12 @@ const Person = props => {
           ))}
         </div>
       </div>
-      <div className="person-img">
+      <div
+        className="person-img"
+        ref={element => {
+          personImg = element;
+        }}
+      >
         <img src={props.imgUrl} alt="person"></img>
       </div>
     </div>
