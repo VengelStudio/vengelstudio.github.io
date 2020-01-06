1b06685c47c4f1cb9bfb8b17fc9fe35907027809
Łukasz Blachnicki <lblachnicki@gmail.com>
2020-01-06 11:10:59 +0100

    Add responsive navbar and toggleable menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,17 +3,24 @@ import "./Navbar.scss";
 import { getClassNameWithSize } from "./Utilities.js";
 import VSLogo from "../../assets/images/vengel-logo.svg";
 import Text from "../Shared/Text/Text";
+import HamburgerMenuIcon from "../../assets/icons/hamburger.svg";
 
 const scrollZoomTrigger = 300;
 
 class Navbar extends React.Component {
   state = {
-    isDownsize: false
+    isDownsize: false,
+    isMenuToggled: false
   };
 
   handleScroll = e => {
     const { scrollTop } = e.srcElement.scrollingElement;
     this.setState({ isDownsize: scrollTop > scrollZoomTrigger });
+  };
+
+  handleMenuToggle = e => {
+    e.preventDefault();
+    this.setState({ isMenuToggled: !this.state.isMenuToggled });
   };
 
   componentDidMount() {
@@ -34,10 +41,12 @@ class Navbar extends React.Component {
             alt="logo"
           ></img>
           <div
-            className={getClassNameWithSize(
-              `navigation-buttons shadow-${this.props.shadow}`,
-              this.state.isDownsize
-            )}
+            className={
+              getClassNameWithSize(
+                `navigation-buttons shadow-${this.props.shadow}`,
+                this.state.isDownsize
+              ) + (this.state.isMenuToggled ? " toggled" : "")
+            }
           >
             <button>
               <Text size={2} weight={300}>
@@ -55,6 +64,9 @@ class Navbar extends React.Component {
               </Text>
             </button>
           </div>
+          <button className="menu-toggle-btn" onClick={this.handleMenuToggle}>
+            <img src={HamburgerMenuIcon} />
+          </button>
         </div>
       </div>
     );
