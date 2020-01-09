717db06048b7eb585d049de2c24733af9cf1e535
Łukasz Blachnicki <lblachnicki@gmail.com>
2020-01-09 08:24:19 +0100

    Update layout scroll triggers

--- src/components/layoutConfig.js
+++ src/components/layoutConfig.js
@@ -1,3 +1,3 @@
 // are units are pixels
-export const navbarScrollTrigger = window.innerHeight * 0.75;
-export const parallaxContainerScrollTrigger = window.innerHeight * 1;
+export const navbarScrollTrigger = window.innerHeight * 0.25;
+export const parallaxContainerScrollTrigger = window.innerHeight * 0.75;
