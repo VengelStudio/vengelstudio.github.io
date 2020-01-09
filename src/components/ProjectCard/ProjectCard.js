debf9c209d369e46127e7d1e370c8f0bcf04cc78
Łukasz Blachnicki <lblachnicki@gmail.com>
2020-01-09 08:55:27 +0100

    Add material UI cards

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,27 +1,52 @@
 import React from "react";
 import "./ProjectCard.scss";
-import Header from "../Shared/Header/Header";
-import Text from "../Shared/Text/Text";
-import Button from "../Button/Button";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 300
+  },
+  media: {
+    height: 240
+  }
+});
 
 const ProjectCard = props => {
+  const classes = useStyles();
   return (
-    <div className="card-wrapper">
-      <div className="project-screenshot">
-        <img className="screenshot" src={props.screenshot}></img>
-      </div>
-      <div className="project-info">
-        <div className="project-title">
-          <Header size={6}>{props.title}</Header>
-        </div>
-        <div className="project-description">
-          <Text size={6}>{props.description}</Text>
-        </div>
-        <Button size={6} type="learn">
-          Learn more..
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={props.screenshot}
+          title="cardman"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Play
         </Button>
-      </div>
-    </div>
+        <Button size="small" color="secondary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
