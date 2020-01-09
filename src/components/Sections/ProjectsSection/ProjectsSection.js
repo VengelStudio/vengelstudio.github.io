bed70591243444def2e2fffb101411baee8694e5
Łukasz Blachnicki <lblachnicki@gmail.com>
2020-01-09 08:55:38 +0100

    Layout adjustments

--- src/components/Sections/ProjectsSection/ProjectsSection.js
+++ src/components/Sections/ProjectsSection/ProjectsSection.js
@@ -4,6 +4,11 @@ import Header from "../../Shared/Header/Header";
 import ProjectCard from "../../ProjectCard/ProjectCard";
 import Cardman from "../../../assets/images/projects/cardman.png";
 
+import Cardman2 from "../../../assets/images/projects/unknown.png";
+import ph1 from "../../../assets/images/projects/kiki-siepel-vRqcK5jydPg-unsplash.jpg";
+import ph2 from "../../../assets/images/projects/michael-milverton-enLhPJDohz8-unsplash.jpg";
+import ph3 from "../../../assets/images/projects/oliver-schwendener-lF7PE1QMbjY-unsplash.jpg";
+
 const ProjectsSection = () => {
   return (
     <section className="projects">
@@ -16,21 +21,21 @@ const ProjectsSection = () => {
           description={
             "A combination of a traditional hangman game and a card game"
           }
-          screenshot={Cardman}
+          screenshot={Cardman2}
         />
         <ProjectCard
           title={"Cardman Multiplayer"}
           description={
             "A combination of a traditional hangman game and a card game"
           }
-          screenshot={Cardman}
+          screenshot={ph2}
         />
         <ProjectCard
           title={"Cardman Multiplayer"}
           description={
             "A combination of a traditional hangman game and a card game"
           }
-          screenshot={Cardman}
+          screenshot={ph3}
         />
       </div>
     </section>
