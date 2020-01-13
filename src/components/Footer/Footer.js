ddd9bb65809ef52dfbc0aa30c11c5fb84bbe4ff5
Bartosz Kępka <bartoszkepka1999@gmail.com>
2020-01-13 08:37:48 +0100

    Add contact links in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,10 @@
 import React from "react";
 import "./Footer.scss";
 import Text from "../Shared/Text/Text";
+import github from "../../assets/icons/github.svg";
 
 const Footer = props => {
-  return (
-    <div className="footer">
-      <Text size={6}>{props.text}</Text>
-    </div>
-  );
+  return <div className="footer">{props.children}</div>;
 };
 
 export default Footer;
--- src/components/Sections/FooterSection/FooterSection.js
+++ src/components/Sections/FooterSection/FooterSection.js
@@ -1,12 +1,29 @@
 import React from "react";
 import "./FooterSection.scss";
 import Footer from "../../Footer/Footer";
+import github from "../../../assets/icons/github.svg";
 
 const FooterSection = () => {
   const currentYear = new Date().getFullYear();
   return (
     <section className="footer">
-      <Footer text={`©${currentYear} Vengel Studio`} />
+      <Footer>
+        <div className="year">{`©${currentYear} Vengel Studio`}</div>
+        <div className="contact">
+          <div className="person">
+            <a href="https://github.com/Dezann">
+              <img src={github}></img>
+              <p>Bartosz Kępka</p>
+            </a>
+          </div>
+          <div className="person">
+            <a href="https://github.com/Kartm">
+              <img src={github}></img>
+              <p>Łukasz Blachnicki</p>
+            </a>
+          </div>
+        </div>
+      </Footer>
     </section>
   );
 };
