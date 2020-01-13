cd3be852148b3d0a3f62dbc97d166fc493ea2488
Łukasz Blachnicki <lblachnicki@gmail.com>
2020-01-13 09:27:44 +0100

    Add parallax and adjust projects section

--- src/components/ParallaxContainer/ParallaxContainer.js
+++ src/components/ParallaxContainer/ParallaxContainer.js
@@ -2,13 +2,37 @@ import React from "react";
 import "./ParallaxContainer.scss";
 
 class ParallaxContainer extends React.Component {
+  containerRef = new React.createRef();
+  state = { bgHeight: "0px" };
+
+  componentDidMount() {
+    this.updateSize();
+
+    const observer = new ResizeObserver(() => {
+      this.updateSize();
+    });
+    observer.observe(this.containerRef.current);
+  }
+
+  updateSize(event) {
+    if (this.containerRef) {
+      this.setState({
+        bgHeight: `${this.containerRef.current.scrollHeight}px`
+      });
+    }
+  }
+
   render() {
     return (
-      <div className="parallax-container">
-        <div className="background">
-          <img src="/static/bg.jpg" alt="background" />
+      <div ref={this.containerRef} className="parallax-container">
+        <div className="parallax-layer --background">
+          <img
+            style={{ height: this.state.bgHeight }}
+            src="/static/bg.jpg"
+            alt="background"
+          />
         </div>
-        <div className="foreground">{this.props.children}</div>
+        <div className="parallax-layer --foreground">{this.props.children}</div>
       </div>
     );
   }
