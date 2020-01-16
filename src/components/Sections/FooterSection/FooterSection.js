5d6b473e696e2d21f5074defd0a592463edddebc
Łukasz Blachnicki <lblachnicki@gmail.com>
2020-01-16 08:18:09 +0100

    Fix footer font

--- src/components/Sections/FooterSection/FooterSection.js
+++ src/components/Sections/FooterSection/FooterSection.js
@@ -1,28 +1,16 @@
 import React from "react";
 import "./FooterSection.scss";
 import Footer from "../../Footer/Footer";
-import github from "../../../assets/icons/github.svg";
+import Text from "../../Shared/Text/Text";
 
 const FooterSection = () => {
   const currentYear = new Date().getFullYear();
   return (
     <section className="footer">
       <Footer>
-        <div className="year">{`©${currentYear} Vengel Studio`}</div>
-        <div className="contact">
-          <div className="person">
-            <a href="https://github.com/Dezann">
-              <img src={github}></img>
-              <p>Bartosz Kępka</p>
-            </a>
-          </div>
-          <div className="person">
-            <a href="https://github.com/Kartm">
-              <img src={github}></img>
-              <p>Łukasz Blachnicki</p>
-            </a>
-          </div>
-        </div>
+        <Text size={6}>
+          <div className="year">{`©${currentYear} Vengel Studio`}</div>
+        </Text>
       </Footer>
     </section>
   );
