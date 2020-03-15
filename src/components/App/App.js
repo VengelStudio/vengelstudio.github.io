6f29db30b4e1df87a8914a28d03fb2b38e87505e
Łukasz Blachnicki <lblachnicki@gmail.com>
2020-03-15 11:15:05 +0100

    Add privacy policy

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,25 +7,36 @@ import ProjectsSection from "../Sections/ProjectsSection/ProjectsSection";
 import ContactSection from "../Sections/ContactSection/ContactSection";
 import FooterSection from "../Sections/FooterSection/FooterSection";
 import CompanyLogo from "../CompanyLogo/CompanyLogo";
+import KtoTerazPrivacyPolicy from "../KtoTerazPrivacyPolicy/KtoTerazPrivacyPolicy";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <ParallaxContainer>
-      <FullHeightWrapper>
-        <CompanyLogo />
-        <Hero>
-          <div className="text-break">We are </div>Vengel Studio
-        </Hero>
-      </FullHeightWrapper>
+    <Router>
+      <Switch>
+        <Route path="/kto-teraz/privacy-policy">
+          <KtoTerazPrivacyPolicy />
+        </Route>
+        <Route path="/">
+          <ParallaxContainer>
+            <FullHeightWrapper>
+              <CompanyLogo />
+              <Hero>
+                <div className="text-break">We are </div>Vengel Studio
+              </Hero>
+            </FullHeightWrapper>
 
-      <FullHeightWrapper>
-        <ProjectsSection />
-      </FullHeightWrapper>
-      <FullHeightWrapper>
-        <ContactSection />
-      </FullHeightWrapper>
-      <FooterSection />
-    </ParallaxContainer>
+            <FullHeightWrapper>
+              <ProjectsSection />
+            </FullHeightWrapper>
+            <FullHeightWrapper>
+              <ContactSection />
+            </FullHeightWrapper>
+            <FooterSection />
+          </ParallaxContainer>
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
